b0dbb802fe1424b2e7c0c314bd06df05221d0ed5
Daniyal Aamir <daamir89@gmail.com>
2024-04-11 10:24:02 -0400

    add layout styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import "antd/dist/antd.css";
+import "./assets/styles/main.css";
+import "./assets/styles/responsive.css";
 
 function App() {
   return (
