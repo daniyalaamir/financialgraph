d914b0da38fdf8f78f51af7d97776bcaae509ea1
Daniyal Aamir <daamir89@gmail.com>
2024-04-13 03:24:33 -0400

    update api with api key

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_ALPHA_VANTAGE_API_BASE_URL;
-let API_KEY = process.env.REACT_APP_ALPHA_VANTAGE_API_KEY;
+let API_KEY = "18LKOMQ81TR6SIBG"
 
 export const getQuarterlyData = async (type, symbol) => {
-  return await axios.get(`${API_BASE_URL}?function=${type}&symbol=${symbol}&apikey=${API_KEY}`);
+  return await axios.get(`https://www.alphavantage.co/query?function=${type}&symbol=${symbol}&apikey=${API_KEY}`);
 }
